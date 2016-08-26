2d3fa0b1603cbd34e96f8c6485004ec757884116
Dariusz Włodarczyk <milley@krotoszyn.pl>
2016-08-26 15:57:22 +0200

    Improvements to zplug installing and loading

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,5 @@
-# btrfs snapshot root filesystem
-alias snapshot-root='sudo btrfs subvolume snapshot -r /run/btrfs-root/__current/ROOT /run/btrfs-root/__snapshot/ROOT@`date "+%Y%m%d-%H%M%S"`'
-
 # photo rename
-alias exif-rename="exiftool -r '-FileName<CreateDate' -d '%Y-%m-%d %H.%M.%S%%-c.%%le'"
+alias imgren2date="exiftool -r '-FileName<CreateDate' -d '%Y-%m-%d %H.%M.%S%%-c.%%le'"
 
 # check my external ip address
 alias checkip="dig +short myip.opendns.com @resolver1.opendns.com"
