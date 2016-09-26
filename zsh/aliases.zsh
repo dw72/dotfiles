aaae7f69870480f7c610ae419435f2f9a4bdfc8a
Dariusz Włodarczyk <milley@krotoszyn.pl>
2016-09-26 15:20:29 +0200

    zplug at extend_glob branch and some new aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,10 @@
+alias dd="dd status=progress"
+
 # photo rename
 alias imgren2date="exiftool -r '-FileName<CreateDate' -d '%Y-%m-%d %H.%M.%S%%-c.%%le'"
 
 # check my external ip address
 alias checkip="dig +short myip.opendns.com @resolver1.opendns.com"
+
+# remote desktop
+alias mstsc="rdesktop -z -PKD -g 1200x720 -k pl -5 -r clipboard:CLIPBOARD -d ZSP2 -u Administrator"
