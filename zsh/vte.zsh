d7b5224965c4c3a56eae8769484c7c999a6b6233
Dariusz Włodarczyk <milley@krotoszyn.pl>
2016-09-27 15:22:16 +0200

    Changed Terminix fix according to documentation

--- zsh/vte.zsh
+++ zsh/vte.zsh
@@ -1,4 +1,4 @@
 # Fix Terminix VTE Configuration Issue. See: https://github.com/gnunn1/terminix/wiki/VTE-Configuration-Issue
-if [[ -f /etc/profile.d/vte.sh ]]; then
+if [[ $TERMINIX_ID ]]; then
   source /etc/profile.d/vte.sh
 fi
