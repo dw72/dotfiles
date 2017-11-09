55601a9d0e5a434fc5bbb3ebdcb6ad96a683858b
Dariusz Włodarczyk <milley@krotoszyn.pl>
2017-11-09 17:19:26 +0100

    Added aliases for ddc control of Samsung Syncmaster 173p monitor

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,10 @@ ls --color -d . &>/dev/null 2>&1 && alias ls='ls -h --color=tty' || alias ls='ls
 
 alias dd="dd status=progress"
 
+#samsung monitor
+alias sam-auto-image="sudo ddcutil setvcp 1e 1; sudo ddcutil setvcp b0 1;"
+alias sam-factory-defaults="sudo ddcutil setvcp 06 1; sudo ddcutil setvcp b0 1;"
+
 # photo rename
 alias exif2name="exiftool -r '-FileName<CreateDate' -d '%Y-%m-%d %H.%M.%S%%-c.%%le'"
 
